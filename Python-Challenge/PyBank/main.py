e27be4630556993b8d1c7405a6a0540a18bb7966
Jeff Eickholt <jteickholt@gmail.com>
2019-10-25 17:29:17 -0500

    uploading final files for homework

--- Python-Challenge/PyBank/main.py
+++ Python-Challenge/PyBank/main.py
@@ -5,6 +5,8 @@ import csv
 
 budget_csv = os.path.join('..','..', '..', 'RU-HOU-DATA-PT-09-2019-U-C', 'Homework', '03-Python', 'Instructions', 'Pybank', 'Resources','budget_data.csv')
 
+#define several variable and set initial values
+
 total=0
 nomonths=0
 change=0
@@ -28,11 +30,17 @@ with open(budget_csv, 'r') as csvfile:
     csvheaer = next(csvreader)
 
     for row in csvreader:
+        #create variables to hold total P/L and total number of months.  Also define rownum to more simply
+        #refer to the row for use later
         total=total + float(row[1])
         nomonths=nomonths+1
         rownum=csvreader.line_num
+        #for first row, there is not previous P/L
         if rownum==2:
             prevpl=0
+        #for other rows, defind the change in P/L and accumulate the total change
+        # Also test to to see if the row contains a bigger increase or decease than previous
+        # which then store the increase/decrease and the month-yr    
         else:
             change=int(row[1])-prevpl
             totchange=totchange + change
@@ -42,14 +50,14 @@ with open(budget_csv, 'r') as csvfile:
             if change<0 and change<greatdec:
                 decmonth=row[0]
                 greatdec=change
-    
+        #set the previous P/L to be used in next loop
         prevpl=int(row[1])
 
-        # print(row, change)
-        
+       
+#average change is total change divided by months -1 since there is no change in the first month       
 avgchange=round(totchange/(nomonths - 1),2)
 
-# print(f"The total P/L is {total} and the number of months is {nomonths}")
+# print the results in the desired format
 
 print("Financial Analysis")
 print("-----------------------------")
@@ -58,16 +66,19 @@ print(f"Total: ${total}")
 print(f"Average Change: ${avgchange}")
 print(f"Greatest Increase in Profits: {incmonth} (${greatinc})")
 print(f"Greatest Decrease in Profits: {decmonth} (${greatdec})")
+
+#change the standard output to a text file and
+# print the results in the desired format
 
 import sys
 
 # f=open('.\pybank.txt', 'w')
 
-f=open('.\practice.txt', 'w')
+f=open('.\pybankresults.txt', 'w')
 std=sys.stdout
 sys.stdout=f
 
-
+print()
 print("Financial Analysis")
 print("-----------------------------")
 print(f"Total Months: {nomonths}")
@@ -76,6 +87,7 @@ print(f"Average Change: ${avgchange}")
 print(f"Greatest Increase in Profits: {incmonth} (${greatinc})")
 print(f"Greatest Decrease in Profits: {decmonth} (${greatdec})")
 
+#make sure to close the file and change the standar output back to the screeen
 f.close()
 sys.stdout=std
 
--- Python-Challenge/PyPoll/main.py
+++ Python-Challenge/PyPoll/main.py
@@ -6,7 +6,7 @@ import csv
 
 election_csv = os.path.join('..','..', '..', 'RU-HOU-DATA-PT-09-2019-U-C', 'Homework', '03-Python', 'Instructions', 'Pypoll', 'Resources','election_data.csv')
 
-# define a candidate list and a vote list
+# define a candidate list and a vote list and a list count that will hold the # of items in list
 
 candlist=[]
 votelist=[]
@@ -19,11 +19,18 @@ with open(election_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csvheader = next(csvreader)
    
+   #for the first row, I will append the candidates name to the list and append 1 vote to the votelist
+    
     for row in csvreader:
         rownum=csvreader.line_num
         if rownum==2:
             candlist.append(row[2])
             votelist.append(1)
+
+        #for other rows, I first check to see if the current candidate is already on the list
+        #if they are, I add a vote
+        #if they are note, I append the name to the candidate list and append 1 to the votelist
+
         else:
             inlist = 0
             for i in range(0 , listcount):
@@ -36,26 +43,57 @@ with open(election_csv, 'r') as csvfile:
                 listcount = listcount + 1
 
 
+# I then add up the number of votes across the candidates
+
 totalvotes=0
-
-
 
 for i in range(len(votelist)):
     totalvotes = totalvotes + int(votelist[i])
 
+#I also define the percentage of votes for each candidate
 
 percvote=[]
-percvotei=0.0
+percvotei=0.000
 for i in range(len(votelist)):
-    percvotei=round((int(votelist[i])/totalvotes)*100 , 3)
-    print(percvotei)
+    percvotei=round((float(votelist[i])/totalvotes)*100 , 3)
     percvote.append(percvotei)
 
 
-# sort the candidates by number of votes.  they are already in the correct order
+# sort the candidates by number of votes.  they are already in the correct order in our data,
 # but this code would fix it if they weren't
 
 candlist, votelist=zip(*sorted(zip(candlist,votelist), key=lambda pair: pair[1], reverse=True))
+
+#print results in desired format
+
+print()
+print("Election Results")
+print()
+print("_"*20)
+print()
+print(f"Total Votes: {totalvotes}")
+print("_"*20)
+print()
+
+for i in range(len(candlist)): 
+    print(f"{candlist[i]} : {percvote[i]}% ({votelist[i]})")
+print()
+print("_"*20)
+print()
+print(f"Winner: {candlist[0]}")
+print()
+print("_"*20)
+
+#change the standard output to print to a text file
+#print results in desired format
+
+import sys
+
+# f=open('.\pybank.txt', 'w')
+
+f=open('.\pypollresults.txt', 'w')
+std=sys.stdout
+sys.stdout=f
 
 #print output
 print()
@@ -76,8 +114,10 @@ print(f"Winner: {candlist[0]}")
 print()
 print("_"*20)
 
+#make sure to close the file and change the standard output back to the screeen
 
-
+f.close()
+sys.stdout=std
 
 
 
